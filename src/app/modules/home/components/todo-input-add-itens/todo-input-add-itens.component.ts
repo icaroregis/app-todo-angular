6ea82b5b2aca88e67ccb7e50edd0891046534edd
Ícaro Almeida <62527468+icaroregis@users.noreply.github.com>
2022-08-25 00:19:28 -0300

    finalização do projeto

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
-  styleUrls: ['./todo-input-add-itens.component.scss']
+  styleUrls: ['./todo-input-add-itens.component.scss'],
 })
 export class TodoInputAddItensComponent implements OnInit {
+  /* output é como se fosse a comunicação mindireta do react onde temos que passar uma props de filho para pai e isso é possível através do output */
+  @Output() public emitItemTaskList = new EventEmitter();
 
-  constructor() { }
+  public addItemTaskList: string = '';
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  public submitItemTaskList() {
+    /* o método trim retira os espaços vazios */
+    this.addItemTaskList = this.addItemTaskList.trim();
+    this.emitItemTaskList.emit(this.addItemTaskList);
+    this.addItemTaskList = '';
   }
-
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,15 +6,26 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent implements OnInit {
-  public taskList: Array<TaskList> = [
-    { task: 'lavar a louça', checked: true },
-    { task: 'Estudar Angular', checked: true },
-  ];
+export class TodoListComponent implements OnInit, DoCheck {
+  public taskList: Array<TaskList> = JSON.parse(
+    localStorage.getItem('list') || '[]'
+  );
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngDoCheck(): void {
+    this.setLocalStorage();
+  }
+  ngOnInit() {}
+
+  public setEmitTaskList(event: string) {
+    /* verificação para caso o usuário queira dicionar tasks vazias sem conteúdo */
+    if (event === '') {
+      return;
+    }
+
+    this.taskList.push({ task: event, checked: false });
+  }
 
   public deleteItemTaskList(event: number) {
     this.taskList.splice(event, 1);
@@ -25,6 +36,26 @@ export class TodoListComponent implements OnInit {
 
     if (confirm) {
       this.taskList = [];
+    }
+  }
+
+  public validationInput(event: string, index: number) {
+    if (event.length === 0) {
+      const confirm = window.confirm('Task está vazia! Deseja Deletar ?');
+
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+
+  public setLocalStorage() {
+    if (this.taskList.length > 0) {
+      this.taskList.sort((first, last): any => {
+        Number(first.checked) - Number(last.checked);
+      });
+
+      localStorage.setItem('list', JSON.stringify(this.taskList));
     }
   }
 }
